187a302b9b90e6002403ecf6d8ebc643a3f3e777
Peter Zheng <scholarpetez@gmail.com>
2020-04-29 22:38:47 -0400

    Switched to using one mutex. Updated function that create processes to create all at once and store in a global process id array.

--- prog1.c
+++ prog1.c
@@ -25,28 +25,35 @@ typedef struct{
 	int r1_sent;
 	int r2_sent;
 
-	pthread_mutex_t r1_received_lock;
-	pthread_mutex_t r1_sent_lock;
-	pthread_mutex_t r2_received_lock;
-	pthread_mutex_t r2_sent_lock;
+	pthread_mutex_t counter_lock;
 
 } Counter;
 
 // Contains the variables used to calculate statistics for reports.
 typedef struct{
 
+	// Records the time of last signal received of the respective time.
 	struct timespec r1_start_time;
 	struct timespec r2_start_time;
+	// The current system time.
 	struct timespec system_time;
 
+	// For testing purposes.
 	int r_count;
+	// Flag for whether to write a report.
 	int write_report;
 
+	// The file descriptor for the report file.
 	int fp;
+	// The total report count that resets when it hits 10.
 	int report_count;
+	// The total SIGUSR1 signal received during 10 signal interval.
 	int r1_report_count;
+	// The total SIGUSR2 signal received during 10 signal interval.
 	int r2_report_count;
+	// The average of the interval between SIGUSR1 received.
 	double r1_time_sum;
+	// The average of the interval between SIGUSR2 received.
 	double r2_time_sum;
 
 } Reporter;
@@ -59,22 +66,35 @@ Reporter* reporter;
 int shm_id_counter;
 int shm_id_reporter;
 
-pid_t* create_handler_processes(int, int);
-pid_t* create_signaler_processes(int);
+// Process ids.
+pid_t processes[8];
+// Function to create processes.
+void create_processes();
 
+// Function for the child process that handles SIGUSR1.
 void r1_handler_process();
+// Function for the child process that handles SIGUSR2.
 void r2_handler_process();
+// Function that for signal generator process.
 void signaler_process();
+// Function for reporter process.
 void reporter_process();
 
+// Handler for handler processes and signaler process.
 void signal_handler(int);
+// Handler for reporter process.
 void signal_report_handler(int);
 
+// Initialize the global counter struct.
 void initialize_counter();
+// Initialize the global reporter struct.
 void initialize_reporter();
+// Sleep for random interval.
 void rand_sleep();
+// Generate a random probability.
 double rand_prob();
 
+// Functions for blocking and unblocking.
 void block_SIGUSR1();
 void unblock_SIGUSR1();
 void block_SIGUSR2();
@@ -84,73 +104,51 @@ void unblock_SIGTERM();
 
 int main(int argc, char* argv[]){
 
+	// Set the seed as the current time.
 	srand(time(NULL));
 
+	// Initialize the shared global counter and reporter.
 	initialize_counter();
 	initialize_reporter();
 
+	// Block SIGUSR1, SIGUSR2 and SIGTERM.
 	block_SIGUSR1();
 	block_SIGUSR2();
 	block_SIGTERM();
 
+	// Attach shared memory for counter.
 	counter = (Counter*) shmat(shm_id_counter, 0, 0);
 
-	pid_t* handler_processes = create_handler_processes(2,2);
+	// Create processes.
+	create_processes();
 
-	reporter = (Reporter*) shmat(shm_id_reporter, 0, 0);
-
-	pid_t report_process = fork();
-	if(reporter_process < 0){
-		printf("Forking Error\n");
-		exit(1);
-	}
-	else if(report_process == 0){
-		reporter_process();
-		exit(1);
-	}
-
-	shmdt(reporter);
-
-	pid_t* signaler_processes = create_signaler_processes(3);
-
+	// If argument for a signal count present.
 	if(argc == 2){
 
 		int total = atoi(argv[1]);
-
+		// Loop until count is not less than total.
 		while((counter->r1_received + counter->r2_received) < total){	
-
 		}
-
-		killpg(0, SIGTERM);
-
 	}else{
-		sleep(30);
-
-		killpg(0, SIGTERM);
+		// Else just sleep for specified time.
+		sleep(5);
+	}
+	
+	// Send SIGTERM to all processes to end all processes.
+	for(int i = 0; i < 8; i++){
+		kill(processes[i], SIGTERM);
 	}
 	
 	int i = 0;
 	int status = 0;
 
-        waitpid(report_process, &status, 0);
-        printf("Reporter Process %d exited with status %d\n", report_process, status);
-
-	while(i <= 2){
-
-                waitpid(signaler_processes[i], &status, 0);
-                printf("Signaler Process %d exited with status %d\n", signaler_processes[i], status);
-                i++;
-        }
-
-	i = 0;
-
-	while(i <= 3){
-		
-		waitpid(handler_processes[i], &status, 0);
-		printf("Handler Process %d exited with status %d\n", handler_processes[i], status);
+	// Wait for all processes to end.
+	while(i != 8){
+		waitpid(processes[i], &status, 0);
 		i++;
 	}
 
+	// Test prints.
 	printf("R1 count: %d\n", counter->r1_received);
 
 	printf("R1 sent: %d\n", counter->r1_sent);
@@ -159,10 +157,9 @@ int main(int argc, char* argv[]){
 	
 	printf("R2 sent: %d\n", counter->r2_sent);
 
+	// Detach and destroy shared memory.
 	shmdt(counter);
 	shmdt(reporter);
-	free(handler_processes);
-	free(signaler_processes);
 	shmctl(shm_id_counter, IPC_RMID, NULL);
 	shmctl(shm_id_reporter, IPC_RMID, NULL);
 
@@ -171,135 +168,161 @@ int main(int argc, char* argv[]){
 
 void r1_handler_process(){
 
+	// Establish signal handler for SIGUSR1 and SIGTERM.
 	signal(SIGUSR1, signal_handler);
 	signal(SIGTERM, signal_handler);
 
-	unblock_SIGUSR1();
-	unblock_SIGTERM();
+	// Set up a wait mask for sigsuspend.
+	sigset_t wait_mask;
+	// Fill the set.
+	sigfillset(&wait_mask);
+	// Delete SIGUSR1.
+	sigdelset(&wait_mask, SIGUSR1);
+	// Delete SIGTERM.
+	sigdelset(&wait_mask, SIGTERM);
 
 	while(1){
-
-		pause();
+		// Now suspends until SIGUSR1 or SIGTERM is received.
+		sigsuspend(&wait_mask);
 	}
 }
 
 void r2_handler_process(){
 
+	// Establish signal handler for SIGUSR2 and SIGTERM.
 	signal(SIGUSR2, signal_handler);
 	signal(SIGTERM, signal_handler);
 
-	unblock_SIGUSR2();
-	unblock_SIGTERM();
+	// Set up a wait mask for sigsuspend.
+	sigset_t wait_mask;
+        sigfillset(&wait_mask);
+	sigdelset(&wait_mask, SIGUSR2);
+	sigdelset(&wait_mask, SIGTERM);
 
 	while(1){
-
-		pause();
+		// Now suspends until SIGUSR2 or SIGTERM is received.
+		sigsuspend(&wait_mask);
 	}
 }
 
+// Signal generator process.
 void signaler_process(){
 
+	// Establish SIGTERM handler. Used to terminate process.
 	signal(SIGTERM, signal_handler);
-	
+	// Unblock SIGTERM.
 	unblock_SIGTERM();
 
 	double prob = 0.0;
+	int signal;
 
 	while(1){
 
+		// Generate rand probability.
 		prob = rand_prob();
 
-		int signal;
-
+		// Determine which signal base off prob.
 		if(prob < 0.5){
 			signal = SIGUSR1;
 		}else{
 			signal = SIGUSR2;
 		}
 
-		killpg(0, signal);
+		// Send the signal to every process in processes.
+		for(int i = 0; i < 5; i++){
+			// Invoke kill system call to request kernel to send signal to the following process.
+			kill(processes[i], signal);
+		}
 
+		// Increment the corresponding count that is mutually exclusive.
 		if(signal == SIGUSR1){
 
-			pthread_mutex_lock(&(counter->r1_sent_lock));
+			pthread_mutex_lock(&(counter->counter_lock));
 		
 			(counter->r1_sent)++;
 
-			pthread_mutex_unlock(&(counter->r1_sent_lock));
+			pthread_mutex_unlock(&(counter->counter_lock));
 		}
-		else{
+		else if(signal == SIGUSR2){
 
-			pthread_mutex_lock(&(counter->r2_sent_lock));
+			pthread_mutex_lock(&(counter->counter_lock));
 
 			(counter->r2_sent)++;
 
-			pthread_mutex_unlock(&(counter->r2_sent_lock));
+			pthread_mutex_unlock(&(counter->counter_lock));
 		}
-		
-	
+		// After each repetition sleep for random interval.
 		rand_sleep();
-		// Invoke kill system call to request kernel to send signal SIGUSR1 to processes in this group.
 	}
 }
 
+// Process for reporter.
 void reporter_process(){
 
+	// Establish signal handlers for SIGTERM, SIGUSR1, and SIGUSR2.
 	signal(SIGTERM, signal_report_handler);
 	signal(SIGUSR1, signal_report_handler);
 	signal(SIGUSR2, signal_report_handler);
 
-	unblock_SIGUSR1();
-	unblock_SIGUSR2();
-	unblock_SIGTERM();
+	// Create mask for sigsuspend.
+	sigset_t wait_mask;
+	sigfillset(&wait_mask);
+	sigdelset(&wait_mask, SIGUSR1);
+	sigdelset(&wait_mask, SIGUSR2);
+	sigdelset(&wait_mask, SIGTERM);
 
-	char message[100];
+	// Stores the message to write to report file.
+	char message[200];
+	// The average of the interval between SIGUSR1 reception.
 	double r1_average = 0.0;
+	// The average of the interval between SIGUSR2 reception.
 	double r2_average = 0.0;
+	// Used to obtain local time in easier read format.
 	struct tm t;
 	time_t current_time;
 
 	while(1){
+	
+		// Suspend until SIGUSR1, SIGUSR2, or SIGTERM is received.
+		sigsuspend(&wait_mask);
 
-		pause();
-
+		// If the flag for write_report is true.
 		if(reporter->write_report){
-
+		
+			// Get the local time.
 			current_time = time(NULL);
-
 			t = *localtime(&current_time);	
 
+			// Calculate average.
 			r1_average = reporter->r1_time_sum / reporter->r1_report_count; 
-
+			// Calculate average.
 			r2_average = reporter->r2_time_sum / reporter->r2_report_count;
 
-			pthread_mutex_lock(&(counter->r1_received_lock));
-			pthread_mutex_lock(&(counter->r2_received_lock));
-			pthread_mutex_lock(&(counter->r1_sent_lock));
-			pthread_mutex_lock(&(counter->r2_sent_lock));
-
+			pthread_mutex_lock(&(counter->counter_lock));
+			// Use sprintf to convert integer and double to string.
 			sprintf(message, "System Time: %d:%d:%d\nSIGUSR1 count: %d SIGUSR2 count: %d\nSIGUSR1 sent: %d  SIGUSR2 sent: %d\nSIGUSR1 Average: %f SIGUSR2 Average: %f\n\n"
 				, t.tm_hour, t.tm_min, t.tm_sec
 				, counter->r1_received, counter->r2_received
 				, counter->r1_sent, counter->r2_sent
 				, r1_average, r2_average);
 	
-			pthread_mutex_unlock(&(counter->r1_received_lock));	
-			pthread_mutex_unlock(&(counter->r2_received_lock)); 
-			pthread_mutex_unlock(&(counter->r1_sent_lock)); 
-			pthread_mutex_unlock(&(counter->r2_sent_lock)); 
+			pthread_mutex_unlock(&(counter->counter_lock));
 
+			// Write the message to report file.
 			write(reporter->fp, message, strlen(message));
 
+			// Reset the values.
 			reporter->r1_report_count = 0;
 			reporter->r2_report_count = 0;
 			reporter->r1_time_sum = 0.0;
 			reporter->r2_time_sum = 0.0;
 		}
-
+		// Set the flag to false again.
 		reporter->write_report = 0;
 	}
 }
 
+// Random sleep for random time between 0.01 and 0.1 seconds.
 void rand_sleep(){
 
 	double interval = ((double)rand() / (double)RAND_MAX) * (0.09) + 0.01;
@@ -308,33 +331,37 @@ void rand_sleep(){
 
         tm.tv_sec = 0;
 
+	// Convert to nanoseconds.
        	tm.tv_nsec = interval * BILLION;
-
+	// Sleep for the corresponding nanoseconds.
         nanosleep(&tm, NULL);
 }
 
+// Generic rand double generator for between 0 and 1.
 double rand_prob(){
 	return ((double) rand() / (double) RAND_MAX);
 }
 
+// The signal handler for handler and signal generator processes.
 void signal_handler(int sig){
 
+	// Increments accordingly to the signal received. If SIGTERM, detach and exit.
 	if(sig == SIGUSR1){
 
-		pthread_mutex_lock(&(counter->r1_received_lock));
+		pthread_mutex_lock(&(counter->counter_lock));
 
        		(counter->r1_received)++;
 
-		pthread_mutex_unlock(&(counter->r1_received_lock));
+		pthread_mutex_unlock(&(counter->counter_lock));
 
 	}
 	else if(sig == SIGUSR2){
 
-        	pthread_mutex_lock(&(counter->r2_received_lock));
+        	pthread_mutex_lock(&(counter->counter_lock));
 
         	(counter->r2_received)++;
 
-        	pthread_mutex_unlock(&(counter->r2_received_lock));
+        	pthread_mutex_unlock(&(counter->counter_lock));
      
 	}
 	else if(sig == SIGTERM){
@@ -343,16 +370,18 @@ void signal_handler(int sig){
 	}
 }
 
+// Signal handler for reporter process.
 void signal_report_handler(int sig){
 
-	if(sig == SIGUSR1 | sig == SIGUSR2){
-		
+	if(sig == SIGUSR1){
+	
 		(reporter->report_count)++;
 
 		(reporter->r_count)++;
-	
+
 		clock_gettime(CLOCK_MONOTONIC, &(reporter->system_time));
 
+		// If reporter count is 10, set the flag.
 		if(reporter->report_count == 10){
 
                         reporter->report_count = 0;
@@ -360,36 +389,53 @@ void signal_report_handler(int sig){
                         reporter->write_report = 1;
                 }
 
+		// Calculate interval between last SIGUSR1 reception.
 		double seconds_passed;
                 double nseconds_passed;
                 double time_passed;
 
-		if(sig == SIGUSR1){
+		seconds_passed = (double) (reporter->system_time.tv_sec - reporter->r1_start_time.tv_sec);
+		nseconds_passed = (double) (reporter->system_time.tv_nsec - reporter->r1_start_time.tv_nsec) / BILLION;
 
-			seconds_passed = (double) (reporter->system_time.tv_sec - reporter->r1_start_time.tv_sec);
-			nseconds_passed = (double) (reporter->system_time.tv_nsec - reporter->r1_start_time.tv_nsec) / BILLION;
-
-			reporter->r1_start_time = reporter->system_time;
+		reporter->r1_start_time = reporter->system_time;
 	
-			time_passed = seconds_passed + nseconds_passed;
+		time_passed = seconds_passed + nseconds_passed;
 
-			reporter->r1_report_count++;
-			reporter->r1_time_sum += time_passed;
-		}
-		else if(sig == SIGUSR2){
+		reporter->r1_report_count++;
+		reporter->r1_time_sum += time_passed;
+	}
+	else if(sig == SIGUSR2){
+		
+		(reporter->report_count)++;
+                
+                (reporter->r_count)++;
+                
+                clock_gettime(CLOCK_MONOTONIC, &(reporter->system_time));
+                
+		// If reporter count is 10, set the flag.
+                if(reporter->report_count == 10){
+                
+                        reporter->report_count = 0;
+                        
+                        reporter->write_report = 1;
+                }       
 
-			seconds_passed = (double) (reporter->system_time.tv_sec - reporter->r2_start_time.tv_sec);
-			nseconds_passed = (double) (reporter->system_time.tv_nsec - reporter->r2_start_time.tv_nsec) / BILLION;
-
-			reporter->r2_start_time = reporter->system_time;
-
-			time_passed = seconds_passed + nseconds_passed;
-
-			reporter->r2_report_count++;
-			reporter->r2_time_sum += time_passed;
-		}
+		double seconds_passed;
+                double nseconds_passed;
+                double time_passed;
+                
+                seconds_passed = (double) (reporter->system_time.tv_sec - reporter->r2_start_time.tv_sec);
+                nseconds_passed = (double) (reporter->system_time.tv_nsec - reporter->r2_start_time.tv_nsec) / BILLION;
+                        
+                reporter->r2_start_time = reporter->system_time;
+                        
+                time_passed = seconds_passed + nseconds_passed;
+                        
+                reporter->r2_report_count++;
+                reporter->r2_time_sum += time_passed;                     
 	}
 	else if(sig == SIGTERM){
+		// If signal is sigterm, detach from all shared memory, report the report count, and exit.
 		shmdt(counter);
 		printf("Reporter Count: %d\n", reporter->r_count);
 		close(reporter->fp);
@@ -415,10 +461,7 @@ void initialize_counter(){
         pthread_mutexattr_init(&(shared_attr));
         pthread_mutexattr_setpshared(&(shared_attr), PTHREAD_PROCESS_SHARED);
 
-        pthread_mutex_init(&(counter->r1_received_lock), &(shared_attr));
-        pthread_mutex_init(&(counter->r1_sent_lock), &(shared_attr));
-	pthread_mutex_init(&(counter->r2_received_lock), &(shared_attr));
-	pthread_mutex_init(&(counter->r2_sent_lock), &(shared_attr));
+	pthread_mutex_init(&(counter->counter_lock), &(shared_attr));
 
 	pthread_mutexattr_destroy(&shared_attr);
 
@@ -428,6 +471,8 @@ void initialize_counter(){
 
 void initialize_reporter(){
 
+	// Initialize and allocate memory for reporter.
+
 	shm_id_reporter = shmget(IPC_PRIVATE, sizeof(Reporter), IPC_CREAT | 0666);
 
 	reporter = (Reporter*) shmat(shm_id_reporter, 0, 0);
@@ -436,11 +481,13 @@ void initialize_reporter(){
 
 	clock_gettime(CLOCK_MONOTONIC, &(reporter->r2_start_time));
 
-        reporter->fp = open("reports.txt", O_WRONLY|O_CREAT|O_TRUNC, S_IRWXU|S_IRWXG|S_IRWXO);
+        reporter->fp = open("process_reports.txt", O_WRONLY|O_CREAT|O_TRUNC, S_IRWXU|S_IRWXG|S_IRWXO);
 
 	reporter->r_count = 0;
 
 	reporter->report_count = 0;
+
+	reporter->write_report = 0;
 
 	reporter->r1_time_sum = 0.0;
 	
@@ -453,68 +500,60 @@ void initialize_reporter(){
 	shmdt(reporter);
 }
 
-pid_t* create_handler_processes(int r1_num, int r2_num){
+// Create the processes.
+void create_processes(){
 
-	pid_t* handler_processes = malloc((r1_num + r2_num) * sizeof(pid_t));
+	// Basically locks each child process into their respective functions.
 
-	if(handler_processes == NULL){
-		printf("Error: Memory Allocation Failed.\n");
-		exit(1);
-	}
+	for(int i = 0; i < 8; i++){
 
-	int i = 0;
+		processes[i] = fork();
 
-	for(; i < r1_num; i++){
-		handler_processes[i] = fork();
-		if(handler_processes[i] < 0){
-			printf("Forking Error\n");
+		if(processes[i] < 0){
+			printf("Error: Forking Error\n");
 			exit(1);
 		}
-		else if(handler_processes[i] == 0){
-			
-			r1_handler_process();
-			
-			exit(1);
-		} 
-	}
-	for(; i < r1_num + r2_num; i++){
-		handler_processes[i] = fork();
-		if(handler_processes[i] < 0){
-			printf("Forking Error\n");
-			exit(1);
-		}
-		else if(handler_processes[i] == 0){
-			r2_handler_process();
-			exit(1);
-		}
-	}
+		else if(processes[i] == 0){
+			if(i == 0){	
+				reporter = (Reporter*) shmat(shm_id_reporter, 0, 0);
+				reporter_process();
+				shmdt(reporter);
+				exit(1);
+			}
+			else if(i == 1){
+				r1_handler_process();
+				exit(1);
+			}
+			else if(i == 2){
+				r1_handler_process();
+				exit(1);
+			}
+			else if(i == 3){
+				r2_handler_process();
+				exit(1);
+			}
+			else if(i == 4){
+				r2_handler_process();
+				exit(1);
+			}
+			else if(i == 5){
+				signaler_process();
+				exit(1);
+			}
+			else if(i == 6){
+				signaler_process();
+				exit(1);
+			}
+			else if(i == 7){
+				signaler_process();
+				exit(1);
+			}
 
-	return handler_processes;
-}
-
-pid_t* create_signaler_processes(int num){
-
-	pid_t* signaler_processes = malloc((num) * sizeof(pid_t));
-
-	if(signaler_processes == NULL){
-		printf("Error: Memory Allocation Failed.\n");
-		exit(1);
-	}
-	
-	for(int i = 0; i < num; i++){
-		signaler_processes[i] = fork();
-		if(signaler_processes[i] < 0){
-			printf("Forking Error\n");
-			exit(1);
-		}
-		else if(signaler_processes[i] == 0){
-			signaler_process();
-			exit(1);
 		}
 	}
-
-	return signaler_processes;
 } 
+
+// Below are generic functions for blocking and unblocking a signal.
 
 void block_SIGUSR1(){
 
